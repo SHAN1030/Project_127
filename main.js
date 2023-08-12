9ff82a6c609742af827599fd86267d9757db4f9a
SHAN1030 <34219879+SHAN1030@users.noreply.github.com>
2023-08-12 09:58:19 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide();
     canvas.center();
-}
+    posenet = ml5.poseNet(video,modelloaded);
+    posenet.on('pose', gotPoses);
+} 
+
 
 function draw(){
     image(video, 0, 0, 600, 600);
@@ -11,6 +14,10 @@ function draw(){
 
 song = "";
 song2 = "";
+leftwristx = "";
+leftwristy = "";
+rightwristx = "";
+rightwristy = "";
 
 function preload(){
     song = loadSound("disco.mp3");
@@ -20,4 +27,21 @@ function preload(){
 
 function play(){
     song.play();
+}
+
+function modelloaded(){
+    console.log("The poseNet is loaded.");
+}
+
+function gotPoses(results)
+{
+if(results.length>0)
+{
+console.log(results);
+leftwristx = results[0].pose.leftWrist.x;
+leftwristy = results[0].pose.leftWrist.y;
+rightwristx = results[0].pose.rightWrist.x;
+rightwristy = results[0].pose.rightWrist.y;
+console.log(leftwristx + leftwristy + rightwristx + rightwristy);
+}
 }
